0f28fcb4d6949d8bb00fa8dbcf62963d1699bccf
kevinherlindo <132327332+kevinherlindo@users.noreply.github.com>
2024-01-12 04:24:19 -0800

    Delete kevin21071017 directory

--- kevin21071017/akuntansi_kevin/admin/modul/mod_karyawan/hasiledit.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "config/koneksi.php";
-
-$sqledit = mysqli_query($koneksi, "update t_karyawan set nama_karyawan='$_POST[nama]', alamat_karyawan='$_POST[alamat]' , nohp_karyawan='$_POST[hp]' where NIK='$_POST[id]' ");
-
-echo "<script>document.location='karyawan.php'</script>"
-
-?>
